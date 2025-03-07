2f47a4b5abf17d72fdf1d22c7c8c03e1a690af14
almostJohn <garcia.johngale@gmail.com>
2025-03-07 15:45:41 +0800

    feat: add whois command

---
+++ src/commands/utility/whois.js
@@ -0,0 +1,81 @@
+const {
+	ChatInputCommandInteraction,
+	Client,
+	time,
+	TimestampStyles,
+	SlashCommandBuilder,
+} = require("discord.js");
+const dayjs = require("dayjs");
+const { colorFromDuration } = require("../../util/generateMemberLog");
+const { color } = require("../../util/color");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("whois")
+		.setDescription(
+			"Displays information about a user, such as created and joined date",
+		)
+		.addUserOption((option) =>
+			option.setName("user").setDescription("The user to look"),
+		),
+	/**
+	 * @param {ChatInputCommandInteraction} interaction
+	 * @param {Client} client
+	 */
+	async execute(interaction, client) {
+		await interaction.deferReply();
+
+		const targetUser = interaction.options.getUser("user") ?? interaction.user;
+
+		const guildMember = await interaction.guild.members
+			.fetch(targetUser.id)
+			.catch(() => null);
+
+		const sinceCreationFormatted = time(
+			dayjs(targetUser.createdTimestamp).unix(),
+			TimestampStyles.RelativeTime,
+		);
+		const creationFormatted = time(
+			dayjs(targetUser.createdTimestamp).unix(),
+			TimestampStyles.ShortDateTime,
+		);
+
+		const descriptionParts = [
+			`• Username: ${targetUser.toString()} - \`${targetUser.tag}\` (${
+				targetUser.id
+			})`,
+			`• Created: ${creationFormatted} (${sinceCreationFormatted})`,
+		];
+
+		if (guildMember.joinedTimestamp) {
+			const sinceJoinedFormatted = time(
+				dayjs(guildMember.joinedTimestamp).unix(),
+				TimestampStyles.RelativeTime,
+			);
+			const joinedFormatted = time(
+				dayjs(guildMember.joinedTimestamp).unix(),
+				TimestampStyles.ShortDateTime,
+			);
+
+			descriptionParts.push(
+				`• Joined: ${joinedFormatted} (${sinceJoinedFormatted})`,
+			);
+		}
+
+		const embed = {
+			author: {
+				name: `${targetUser.tag} (${targetUser.id})`,
+				icon_url: targetUser.displayAvatarURL(),
+			},
+			color: true
+				? colorFromDuration(Date.now() - guildMember.joinedTimestamp)
+				: color.DarkButNotBlack,
+			description: descriptionParts.join("\n"),
+			timestamp: new Date().toISOString(),
+		};
+
+		await interaction.editReply({
+			embeds: [embed],
+		});
+	},
+};
